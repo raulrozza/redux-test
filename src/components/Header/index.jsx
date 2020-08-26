4ffa18bf4208437ec781aa291215d62cdd8cc8b9
Raul Rosá <raul.hmetal@gmail.com>
2020-08-26 17:13:37 -0300

    Created logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link, useHistory } from 'react-router-dom';
 
+// Icons
+import {
+  FaHome,
+  FaSignInAlt,
+  FaUserAlt,
+  FaCircle,
+  FaPowerOff,
+} from 'react-icons/fa';
+
+// Styles
 import { Nav } from './styles';
 
+// Redux
+import * as actions from '../../store/modules/auth/actions';
+import { useSelector, useDispatch } from 'react-redux';
+
 function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const { push } = useHistory();
+
+  const handleLogout = event => {
+    event.preventDefault();
+
+    dispatch(actions.loginFailure());
+    push('/');
+  };
+
   return (
     <Nav>
       <Link to="/">
         <FaHome />
       </Link>
-      <Link to="/register">
+      <Link to="/register" title={isLoggedIn ? 'Alterar dados' : 'Criar conta'}>
         <FaUserAlt />
       </Link>
-      <Link to="/login">
-        <FaSignInAlt />
-      </Link>
+
+      {isLoggedIn ? (
+        <>
+          <Link to="" onClick={handleLogout} title="Sair">
+            <FaPowerOff />
+          </Link>
+          <FaCircle color="#66ff33" />
+        </>
+      ) : (
+        <Link to="/login" title="Fazer login">
+          <FaSignInAlt />
+        </Link>
+      )}
     </Nav>
   );
 }
